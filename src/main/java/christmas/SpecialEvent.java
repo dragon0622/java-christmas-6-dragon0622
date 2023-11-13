35e010b0484b8356149dd5830d20d9c4e7361233
dragon0622 <iamdragon.dev@gmail.com>
2023-11-13 13:52:45 +0900

    feat(christmas): SpecialEvent 클래스 구현

---
+++ src/main/java/christmas/SpecialEvent.java
@@ -0,0 +1,9 @@
+package christmas;
+
+public class SpecialEvent implements Discount{
+    private static final int SPECIAL_SALE_AMOUNT = 1000;
+    @Override
+    public int doSale(int originalPrice) {
+        return originalPrice - SPECIAL_SALE_AMOUNT;
+    }
+}
