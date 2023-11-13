334888cc60c7eae2db83020b42a32069253f6251
dragon0622 <iamdragon.dev@gmail.com>
2023-11-13 13:43:31 +0900

    feat(christmas): ChristmasEvent 클래스 구현

---
+++ src/main/java/christmas/ChristmasEvent.java
@@ -0,0 +1,16 @@
+package christmas;
+
+public class ChristmasEvent implements Discount{
+    private int salePrice = 0;
+    private static final int START_SALE_AMOUNT = 1000;
+    private static final int INCREASE_SALE_AMOUNT = 100;
+
+    public int findSalePrice(int visitDate){
+        return START_SALE_AMOUNT + (visitDate - 1) * INCREASE_SALE_AMOUNT;
+    }
+
+    @Override
+    public int doSale(int originalPrice) {
+        return originalPrice - salePrice;
+    }
+}
