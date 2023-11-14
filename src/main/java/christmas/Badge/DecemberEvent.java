9169f14e94c0ba38ed89de88f4da736f8879f64e
dragon0622 <iamdragon.dev@gmail.com>
2023-11-14 20:21:06 +0900

    feat(christmas): Create DecemberEvent

---
+++ src/main/java/christmas/Badge/DecemberEvent.java
@@ -0,0 +1,36 @@
+package christmas.Badge;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DecemberEvent implements BadgeGrant{
+
+    private final String EVENT_NAME = "12월 이벤트 배지";
+    private static final Map<Integer, String> BADGES = new TreeMap<>();
+    static {
+        BADGES.put(5000,"별");
+        BADGES.put(10000,"트리");
+        BADGES.put(20000,"산타");
+    }
+    private static String badge = "";
+    @Override
+    public void doGrant(int totalSalePrice, int totalGiftPrice) {
+        int benefitPrice = totalSalePrice + totalGiftPrice;
+
+        for (Map.Entry<Integer, String> pair : BADGES.entrySet()) {
+            if (benefitPrice >= pair.getKey()){
+                badge = pair.getValue();
+            }
+        }
+    }
+
+    @Override
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+
+    @Override
+    public String getBadge() {
+        return badge;
+    }
+}
