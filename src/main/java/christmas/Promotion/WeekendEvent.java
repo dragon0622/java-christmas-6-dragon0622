175975512d5fea1386718ffa5c1c72c9cd1e9747
dragon0622 <iamdragon.dev@gmail.com>
2023-11-15 19:57:02 +0900

    feat(Christmas): 주말 할인 구현

--- src/main/java/christmas/Promotion/WeekendEvent.java
+++ src/main/java/christmas/Promotion/WeekendEvent.java
@@ -1,5 +1,9 @@
 package christmas.Promotion;
 
+import christmas.IllegalArgumentExceptionHandler;
+import christmas.domain.Menu.MainDish;
+import christmas.domain.Menu.Menu;
+
 public class WeekendEvent implements Discount{
     private final String EVENT_NAME = "주말 할인";
     private static final int MAIN_SALE_AMOUNT = 2023;
@@ -7,22 +11,41 @@ public class WeekendEvent implements Discount{
 
 
     @Override
+    public void doSale() {
+    }
+
+    @Override
     public void doSale(int visitDate) {
     }
 
     @Override
-    public void doSale(String[][] mainMenuArray) {
+    public void doSale(String[][] order) {
         int numberOfMain = 0;
-        try{
-            for (String count : mainMenuArray[0]){
-                numberOfMain += Integer.parseInt(count);
-            }
 
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException();
+        for(String[] menus : order){
+            String name = menus[0];
+            String quantity = menus[1];
+            for (Menu menu : Menu.getAllItems()){
+                numberOfMain += countMain(name, quantity, menu);
+            }
         }
 
         salePrice = MAIN_SALE_AMOUNT * numberOfMain;
+    }
+
+    private int countMain(String name, String quantity, Menu menu) {
+        try{
+            if (menu.getName().equals(name)){
+                if (menu instanceof MainDish){
+                    return Integer.parseInt(quantity);
+                }
+            }
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(name + "의 개수가 정수가 아닙니다.");
+        } catch (IllegalArgumentException e){
+            new IllegalArgumentExceptionHandler(e.getMessage());
+        }
+        return 0;
     }
 
     @Override
@@ -33,9 +56,5 @@ public class WeekendEvent implements Discount{
     @Override
     public int getSalePrice() {
         return salePrice;
-    }
-
-    @Override
-    public void doSale() {
     }
 }
