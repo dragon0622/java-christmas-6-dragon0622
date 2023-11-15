bdca270b99f928bac0cb9a0ff23c59a0ef681448
dragon0622 <iamdragon.dev@gmail.com>
2023-11-15 19:58:02 +0900

    feat(Christmas): 증정관련 메소드 처리

--- src/main/java/christmas/GiveAwayValidator.java
+++ src/main/java/christmas/GiveAwayValidator.java
@@ -1,9 +1,11 @@
 package christmas;
 
 import christmas.Badge.BadgeGrant;
+import christmas.GiveAway.GiveAway;
 
 public class GiveAwayValidator {
-    public static void comparePrice(BadgeGrant badgeGrantEvent, int benefitPrice){
-        badgeGrantEvent.doGrant(benefitPrice);
+    private static final int CHAMPAGNE_EVENT_MINIMUM_ORIGINAL_PRICE = 120000;
+    public static void comparePrice(GiveAway giveAwayEvent, int originalPrice){
+        giveAwayEvent.doGiveAway(originalPrice);
     }
 }
