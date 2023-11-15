65cf30b1b409e451a4a5bd5bf01c8ae3b32199f8
dragon0622 <iamdragon.dev@gmail.com>
2023-11-15 20:04:03 +0900

    feat(Christmas): main 프로그램 동작 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,21 @@
 package christmas;
 
+import christmas.View.InputView;
+import christmas.View.OutputView;
+
 public class Application {
+    public static int VISIT_DATE;
+    public static String[][] ORDER;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.startMessage();
+
+        VISIT_DATE = InputView.readDate();
+        ORDER = InputView.readOrder();
+
+        OutputView.eventInformation(VISIT_DATE);
+
+        Planner planner = new Planner(VISIT_DATE, ORDER);
+        planner.createEventPlanner(ORDER);
+
     }
 }
